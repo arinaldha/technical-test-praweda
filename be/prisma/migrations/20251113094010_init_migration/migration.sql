d02f62d45bdd8e7eb034e79f8810797598fad400
Arinal Dzikrul <arinal.dzikrul@praweda.id>
2025-11-13 23:26:53 +0700

    update schema

--- be/prisma/migrations/20251113094010_init_migration/migration.sql
+++ be/prisma/migrations/20251113094010_init_migration/migration.sql
@@ -2,7 +2,7 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 -- CreateTable
-CREATE TABLE "schema_arinal_3"."users" (
+CREATE TABLE "schema_arinal"."users" (
     "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "name" TEXT NOT NULL,
     "username" TEXT NOT NULL,
@@ -24,7 +24,7 @@ CREATE TABLE "schema_arinal_3"."users" (
 );
 
 -- CreateTable
-CREATE TABLE "schema_arinal_3"."employees" (
+CREATE TABLE "schema_arinal"."employees" (
     "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "name" TEXT NOT NULL,
     "code" TEXT NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE "schema_arinal_3"."employees" (
 );
 
 -- CreateTable
-CREATE TABLE "schema_arinal_3"."roles" (
+CREATE TABLE "schema_arinal"."roles" (
     "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "name" TEXT NOT NULL,
 
@@ -47,7 +47,7 @@ CREATE TABLE "schema_arinal_3"."roles" (
 );
 
 -- CreateTable
-CREATE TABLE "schema_arinal_3"."companies" (
+CREATE TABLE "schema_arinal"."companies" (
     "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "code" TEXT NOT NULL,
     "name" TEXT NOT NULL,
@@ -62,7 +62,7 @@ CREATE TABLE "schema_arinal_3"."companies" (
 );
 
 -- CreateTable
-CREATE TABLE "schema_arinal_3"."cities" (
+CREATE TABLE "schema_arinal"."cities" (
     "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "name" TEXT NOT NULL,
     "province_id" UUID NOT NULL,
@@ -71,7 +71,7 @@ CREATE TABLE "schema_arinal_3"."cities" (
 );
 
 -- CreateTable
-CREATE TABLE "schema_arinal_3"."provinces" (
+CREATE TABLE "schema_arinal"."provinces" (
     "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "name" TEXT NOT NULL,
     "country_id" UUID NOT NULL,
@@ -80,7 +80,7 @@ CREATE TABLE "schema_arinal_3"."provinces" (
 );
 
 -- CreateTable
-CREATE TABLE "schema_arinal_3"."countries" (
+CREATE TABLE "schema_arinal"."countries" (
     "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "name" TEXT NOT NULL,
 
@@ -88,7 +88,7 @@ CREATE TABLE "schema_arinal_3"."countries" (
 );
 
 -- CreateTable
-CREATE TABLE "schema_arinal_3"."positions" (
+CREATE TABLE "schema_arinal"."positions" (
     "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "name" TEXT NOT NULL,
 
@@ -96,7 +96,7 @@ CREATE TABLE "schema_arinal_3"."positions" (
 );
 
 -- CreateTable
-CREATE TABLE "schema_arinal_3"."user_roles" (
+CREATE TABLE "schema_arinal"."user_roles" (
     "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "user_id" UUID NOT NULL,
     "role_id" UUID NOT NULL,
@@ -106,7 +106,7 @@ CREATE TABLE "schema_arinal_3"."user_roles" (
 );
 
 -- CreateTable
-CREATE TABLE "schema_arinal_3"."user_companies" (
+CREATE TABLE "schema_arinal"."user_companies" (
     "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "user_id" UUID NOT NULL,
     "position_id" UUID NOT NULL,
@@ -116,37 +116,37 @@ CREATE TABLE "schema_arinal_3"."user_companies" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "users_username_key" ON "schema_arinal_3"."users"("username");
+CREATE UNIQUE INDEX "users_username_key" ON "schema_arinal"."users"("username");
 
 -- AddForeignKey
-ALTER TABLE "schema_arinal_3"."users" ADD CONSTRAINT "users_city_id_fkey" FOREIGN KEY ("city_id") REFERENCES "schema_arinal_3"."cities"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "schema_arinal"."users" ADD CONSTRAINT "users_city_id_fkey" FOREIGN KEY ("city_id") REFERENCES "schema_arinal"."cities"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "schema_arinal_3"."users" ADD CONSTRAINT "users_province_id_fkey" FOREIGN KEY ("province_id") REFERENCES "schema_arinal_3"."provinces"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "schema_arinal"."users" ADD CONSTRAINT "users_province_id_fkey" FOREIGN KEY ("province_id") REFERENCES "schema_arinal"."provinces"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "schema_arinal_3"."users" ADD CONSTRAINT "users_country_id_fkey" FOREIGN KEY ("country_id") REFERENCES "schema_arinal_3"."countries"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "schema_arinal"."users" ADD CONSTRAINT "users_country_id_fkey" FOREIGN KEY ("country_id") REFERENCES "schema_arinal"."countries"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "schema_arinal_3"."cities" ADD CONSTRAINT "cities_province_id_fkey" FOREIGN KEY ("province_id") REFERENCES "schema_arinal_3"."provinces"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "schema_arinal"."cities" ADD CONSTRAINT "cities_province_id_fkey" FOREIGN KEY ("province_id") REFERENCES "schema_arinal"."provinces"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "schema_arinal_3"."provinces" ADD CONSTRAINT "provinces_country_id_fkey" FOREIGN KEY ("country_id") REFERENCES "schema_arinal_3"."countries"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "schema_arinal"."provinces" ADD CONSTRAINT "provinces_country_id_fkey" FOREIGN KEY ("country_id") REFERENCES "schema_arinal"."countries"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "schema_arinal_3"."user_roles" ADD CONSTRAINT "user_roles_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "schema_arinal_3"."users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "schema_arinal"."user_roles" ADD CONSTRAINT "user_roles_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "schema_arinal"."users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "schema_arinal_3"."user_roles" ADD CONSTRAINT "user_roles_role_id_fkey" FOREIGN KEY ("role_id") REFERENCES "schema_arinal_3"."roles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "schema_arinal"."user_roles" ADD CONSTRAINT "user_roles_role_id_fkey" FOREIGN KEY ("role_id") REFERENCES "schema_arinal"."roles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "schema_arinal_3"."user_roles" ADD CONSTRAINT "user_roles_company_id_fkey" FOREIGN KEY ("company_id") REFERENCES "schema_arinal_3"."companies"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "schema_arinal"."user_roles" ADD CONSTRAINT "user_roles_company_id_fkey" FOREIGN KEY ("company_id") REFERENCES "schema_arinal"."companies"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "schema_arinal_3"."user_companies" ADD CONSTRAINT "user_companies_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "schema_arinal_3"."users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "schema_arinal"."user_companies" ADD CONSTRAINT "user_companies_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "schema_arinal"."users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "schema_arinal_3"."user_companies" ADD CONSTRAINT "user_companies_company_id_fkey" FOREIGN KEY ("company_id") REFERENCES "schema_arinal_3"."companies"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "schema_arinal"."user_companies" ADD CONSTRAINT "user_companies_company_id_fkey" FOREIGN KEY ("company_id") REFERENCES "schema_arinal"."companies"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "schema_arinal_3"."user_companies" ADD CONSTRAINT "user_companies_position_id_fkey" FOREIGN KEY ("position_id") REFERENCES "schema_arinal_3"."positions"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "schema_arinal"."user_companies" ADD CONSTRAINT "user_companies_position_id_fkey" FOREIGN KEY ("position_id") REFERENCES "schema_arinal"."positions"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
